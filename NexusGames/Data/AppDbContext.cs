a4330e4fad0483c64fb0cb5098a6d54b7a6af80b
maximapeldimov <maximapeldimov@gmail.com>
2025-11-24 15:21:57 +0200

    2525

---
+++ NexusGames/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusGames.Models;
+using System.Collections.Generic;
+
+
+namespace NexusGames.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Gamer> Gamers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet<TEntity> properties here, for example:
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
